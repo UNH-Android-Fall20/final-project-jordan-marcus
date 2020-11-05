6456d8f1b7b73f3f063e3f3e077411ee9d078e18
marcusnovoa <marcus@marcuscodes.com>
2020-11-05 12:25:16 -0500

    Add EventAdd, EventView, and SeniorTechHelp activities

--- app/src/main/java/dev/jzdevelopers/cstracker/event/EventView.kt
+++ app/src/main/java/dev/jzdevelopers/cstracker/event/EventView.kt
@@ -1,12 +1,41 @@
 package dev.jzdevelopers.cstracker.event
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import dev.jzdevelopers.cstracker.R
+import dev.jzdevelopers.cstracker.libs.JZActivity
+import kotlinx.android.synthetic.main.ui_event_view.*
 
-class EventView : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.ui_event_view)
+class EventView : JZActivity() {
+
+    /**.
+     * What Happens When The Activity Is Created
+     */
+    override fun createActivity() {
+
+        // Creates The UI//
+        createUI(R.layout.ui_event_view) {
+
+            // Sets The Theme For The Activity//
+            theme(R.style.GreenTheme)
+        }
+    }
+
+    /**.
+     * Function That Handles All Listeners For The Activity
+     */
+    override fun createListeners() {
+
+        // When addEventTextView Is Clicked//
+        click(addEventTextView) {
+
+            // Starts The Activity EventAdd//
+            startActivity(EventAdd::class, R.anim.faze_in, R.anim.faze_out)
+        }
+
+        // When seniorTextView Is Clicked//
+        click(seniorTextView) {
+
+            // Starts The Activity SeniorTechHelp//
+            startActivity(SeniorTechHelp::class, R.anim.faze_in, R.anim.faze_out)
+        }
     }
 }
